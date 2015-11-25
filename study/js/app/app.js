d8d99febe205b1968fbcc9b36d1925f96034d885
saumya <2saumyaray@gmail.com>
2015-11-25 16:16:55 +0530

    Studying REACT

--- study/js/app/app.js
+++ study/js/app/app.js
@@ -1,5 +1,5 @@
 // version 0.1.0
 React.render(
-  React.DOM.h1({ id:"hID" },"Hello World. This is REACT !"),
+  React.DOM.h1({ id:"hID", className:"greetClass" },"Hello World. This is REACT !"),
   document.getElementById("myReactApp")
 );
