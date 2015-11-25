3636c5010777a787876bcd6cf32c044a30101178
saumya <2saumyaray@gmail.com>
2015-11-25 16:56:02 +0530

    WIP Component

--- study/js/app/app.js
+++ study/js/app/app.js
@@ -3,3 +3,19 @@ React.render(
   React.DOM.h1({ id:"hID", className:"greetClass" },"Hello World. This is REACT !"),
   document.getElementById("myReactApp")
 );
+// Component
+// var Component1 = React.createClass({});
+var Component1 = React.createClass({
+  render: function(){
+    var reactComponent = React.DOM.span(null,"I am a Custom Span.");
+    return reactComponent;
+  }
+});
+// Component Factory
+var Component1Factory = React.createFactory(Component1);
+/*
+// Using the component
+React.render( React.createElement(Component1), document.getElementById("myReactApp") );
+*/
+// Using the ComponentFactory
+React.render( Component1Factory(), document.getElementById("myReactApp") );
